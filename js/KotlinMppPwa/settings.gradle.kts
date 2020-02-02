a65053fe57b7f17506474e1e01b256e2526668bf
Eric Loew <ericloew@hotmail.com>
2020-02-02 06:53:05 -0600

    Gradle Changes

---
+++ js/KotlinMppPwa/settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "KotlinMppPwa"
+pluginManagement {
+    repositories {
+        jcenter()
+        gradlePluginPortal()
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+
+        mavenCentral()
+
+        maven(url = "https://plugins.gradle.org/m2/")
+    }
+}
+
