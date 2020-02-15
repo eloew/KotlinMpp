0a945b4b8863e16f692e2950cd57a1701a201ad7
Eric Loew <ericloew@hotmail.com>
2020-02-15 07:36:29 -0600

    pwa install button not showing

--- js/KotlinMppReact/src/main/kotlin/view/Application.kt
+++ js/KotlinMppReact/src/main/kotlin/view/Application.kt
@@ -6,10 +6,14 @@ import react.*
 import react.dom.div
 import react.dom.h2
 import kotlinx.css.*
+import kotlinx.html.ButtonType
+import kotlinx.html.id
+import kotlinx.html.hidden
+import react.dom.button
 import react.dom.img
 import styled.*
 import styled.StyledComponents.css
-
+import react.dom.button
 
 interface ApplicationProps : RProps {
     var coroutineScope: CoroutineScope
@@ -24,24 +28,46 @@ class ApplicationComponent : RComponent<ApplicationProps, RState>() {
        //require("bootstrap/dist/js/bootstrap.js")
     }
 
+    object ComponentStyles : StyleSheet("ComponentStyles", isStatic = true) {
+        val install by css {
+            width  =  50.px
+            height = 50.px
+            paddingRight = 25.px
+            float = Float.right
+            backgroundColor = Color.blue
+        }
+
+    }
+
     private val coroutineContext
         get() = props.coroutineScope.coroutineContext
 
     override fun RBuilder.render() {
-        styledImg {
+        styledDiv {
             css {
-                width  =  50.px
-                height = 50.px
+                padding(vertical = 16.px)
+
+                backgroundColor = Color.green
             }
-            attrs {
+            styledImg {
+                css {
+                    width  =  50.px
+                    height = 50.px
+                }
+                attrs {
 
-                src  = "Kotlin-logo.svg"
+                    src  = "Kotlin-logo.svg"
+                }
             }
-        }
-        img {
+            button(classes = "install") {
+                attrs {
+                    id = "butInstall"
+                    hidden = true
+                }
+            }
 
-
         }
+
         div {
             h2 {
                 +"Hello KotlinMpp React"
